029c531875a7e0514e523cdb6c06c0189896ec7e
João Bittencourt <j.v_dias@hotmail.com>
2024-06-24 19:11:47 -0300

    init

---
+++ android/app/src/main/kotlin/com/nutrio/nutrio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nutrio.nutrio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: WebViewScreen(),
+  ));
+}
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({Key? key}) : super(key: key);
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+class _WebViewScreenState extends State<WebViewScreen> {
+  late WebViewController controller;
+  var loadingPercentage = 0;
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: (url) {
+          setState(() {
+            loadingPercentage = 0;
+          });
+        },
+        onProgress: (progress) {
+          setState(() {
+            loadingPercentage = progress;
+          });
+        },
+        onPageFinished: (url) {
+          setState(() {
+            loadingPercentage = 100;
+          });
+        },
+      ))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(
+        Uri.parse('https://teste.alphaaprova.com.br'),
+      );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+              child: WebViewWidget(controller: controller)
+          ),
+          loadingPercentage < 100
+              ? LinearProgressIndicator(
+                color: Colors.red,
+                value: loadingPercentage / 100.0,
+              )
+              : Container()
+        ],
+      ),
+    );
+  }
+}
